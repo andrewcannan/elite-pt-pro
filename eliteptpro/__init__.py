b5e43db1e5bcb61dba60abc5d9cb74845533737a
andrewcannan <>
2023-07-01 14:36:19 +0000

    setup routes and run file, add route to base html file

--- eliteptpro/__init__.py
+++ eliteptpro/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env # oqa
+    import env  # noqa
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -10,4 +10,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy()
 
-from elite_pt_pro import routes # noqa
+from eliteptpro import routes  # noqa
