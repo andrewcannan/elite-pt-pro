3661526865af4d1543453e2ca418df5e3836d383
andrewcannan <>
2023-07-01 19:33:20 +0000

    create model for user table in db

--- eliteptpro/models.py
+++ eliteptpro/models.py
@@ -4,20 +4,23 @@ from eliteptpro import db
 class User(db.Model):
     # schema for User model
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=True)
+    password = db.Column(db.String, unique=True, nullable=True)
+    is_pt = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         # represents itself in the form of a string
-        return self
+        return f"{self.id} - {self.username} | PT: {self.is_pt}"
 
 
 class Trainers(db.Model):
     # schema for Trainers model
     id = db.Column(db.Integer, primary_key=True)
-    trainer_name = db.Column(db.String(80), nullable=False)
+    trainer_name = db.Column(db.String, nullable=False)
     holidays = db.relationship(
-        "Holidays", backref="trainers", cascade="all, delete")
+        "Holidays", backref="trainers", cascade="all, delete", lazy=True)
     sessions = db.relationship(
-        "Sessions", backref="trainers", cascade="all, delete")
+        "Sessions", backref="trainers", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # represents itself in the form of a string
