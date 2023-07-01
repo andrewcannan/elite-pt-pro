0ee7e39bc68fd5dea74f68e9e41bf3f822075f37
andrewcannan <>
2023-07-01 21:37:28 +0000

    add navbar to base.html, logo image for nav, stylesheet, and script sheet

---
+++ eliteptpro/static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav, {
+        closeOnClick: true,
+        edge: 'right'
+    });
+});
