3fdbf29c7914c53a123111434390c5b33b2b9500
andrewcannan <>
2023-07-02 13:16:47 +0000

    create dpostgres db, create register.html and route for page

--- eliteptpro/routes.py
+++ eliteptpro/routes.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect, request, flash, url_for
 from eliteptpro import app, db
+from eliteptpro.models import User, Trainers, Holidays, Sessions
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
 def home():
     return render_template("homepage.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
